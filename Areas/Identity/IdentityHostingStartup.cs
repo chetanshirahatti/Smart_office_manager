232f517323f93cf20830eb8cc1c0822915afb147
Chetan Shirahatti <chetanpshi9@gmail.com>
2025-12-19 18:01:57 +0530

    created Authentication for new user for Register and Login

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+[assembly: HostingStartup(typeof(SmartOfficeManager.Areas.Identity.IdentityHostingStartup))]
+namespace SmartOfficeManager.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            // No configuration needed here,
+            // Identity UI just needs this file to activate.
+        }
+    }
+}
--- obj/Debug/net8.0/SmartOfficeManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/SmartOfficeManager.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartOfficeManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5805f5dd0dda15b4e148ee06b138e8fd7fe1d2d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartOfficeManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartOfficeManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

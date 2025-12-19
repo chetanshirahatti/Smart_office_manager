5805f5dd0dda15b4e148ee06b138e8fd7fe1d2d8
Chetan Shirahatti <chetanpshi9@gmail.com>
2025-12-19 13:28:07 +0530

    Created simple C# project with .Netframework

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartOfficeManager.Models;
+
+namespace SmartOfficeManager.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartOfficeManager.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Department { get; set; }
+
+        public decimal Salary { get; set; }
+    }
+}
